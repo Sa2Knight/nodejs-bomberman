1836fb32efe664f049c21cc1be1c0514015a7935
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-12-04 11:16:44 +0900

    サンプルブログアプリを削除し、チャットアプリの雛形を作成

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var app = express();
-var blog = require('./routes/blog');
+var chatRouter = require('./routes/chatRouter');
 
 // ビュー設定
 app.set('views' , __dirname + '/views');
@@ -19,14 +19,7 @@ app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
 
 // ルーティング
-app.get('/' , blog.index);
-app.get('/posts/new' , blog.new);
-app.post('/posts/create' , blog.create);
-app.get('/posts/:id' , blog.show);
-app.get('/posts/:id/edit' , blog.edit);
-app.post('/posts/:id/edit' , blog.update);
-app.post('/posts/delete' , blog.destroy);
+app.get('/' , chatRouter.index);
 
-// リクエスト受付開始
 app.listen(8081);
 console.log('server starting...');
