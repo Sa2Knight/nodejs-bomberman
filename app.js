cf53b7a213afaa1b6cc7270ab33a757409db5268
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-12-04 13:51:45 +0900

    ミニチャットを破棄して、ボンバーマンの雛形準備

--- app.js
+++ app.js
@@ -18,26 +18,21 @@ app.use(bodyParser.urlencoded({extended: true}));
 //app.use(express.methodOverride());
 
 // ルーティングルール
-var chatRouter = require('./routes/chatRouter');
+var router = require('./routes/router');
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
 
 // ルーティング
-app.get('/' , chatRouter.index);
+app.get('/' , router.index);
 
 // サーバ起動
 var server = http.createServer(app);  //expressを用いてWebサーバ準備
 server.listen(8081);                  //Webサーバを8081番ポートで起動
 var io = socketIO.listen(server);     //socket.io起動
 
-// [イベント] クライアントとの接続確立
 io.sockets.on('connection' , function(socket) {
-  // [イベント] メッセージ受信
   socket.on('message' , function(data) {
-    io.sockets.emit('message' , { value: data.value });
   });
-  // [イベント] 切断
   socket.on('disconnect' , function() {
-    console.log('disconnected');
   });
 });
